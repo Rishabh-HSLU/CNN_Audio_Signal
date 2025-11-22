af0a68dc3bec81b8b319065e89b1a803df02f793
rishabh <rishabh.yadav@stud.hslu.ch>
2025-11-22 23:14:50 +0100

    updated the MooNet class. added two activation function classes In testing.ipynb, calculated the cross entropy loss of the output using two dense layers

--- moonet.py
+++ moonet.py
@@ -1,11 +1,20 @@
 import numpy as np
 
-class Moonet:
+class MooNet:
     def __init__(self, n_inputs, n_neurons: int):
-        self.weights = np.random.normal(0,1,(n_inputs, n_neurons))
-        self.bias = np.zeros(n_neurons)
+        self.weights = 0.01 * np.random.randn(n_inputs, n_neurons)
+        self.biases = np.zeros((1,n_neurons))
 
-    def forward_pass(self, x: np.ndarray) -> np.ndarray:
-        return np.dot(x,self.weights.T) + self.bias
+    def forward_pass(self, inputs: np.ndarray) -> np.ndarray:
+        self.output = np.dot(inputs,self.weights) + self.biases
 
 
+class Relu:
+    def forward_pass(self, inputs: np.ndarray) -> np.ndarray:
+        self.output = np.maximum(0,inputs)
+
+class SoftMax:
+    def forward_pass(self, inputs: np.ndarray):
+        exp_norm = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        probabilities = exp_norm / np.sum(exp_norm, axis=1, keepdims=True)
+        self.output = probabilities
